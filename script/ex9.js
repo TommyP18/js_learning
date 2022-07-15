e8903d06de733645871748f8306a7822f7314b51
Nichita <bodrugnikita18@gmail.com>
2022-07-15 20:02:37 +0300

    last commit All exercises are done

---
+++ script/ex9.js
@@ -0,0 +1,66 @@
+const handleClick = () => {
+  const days = parseInt(document.getElementById("days").value, 0);
+  const hours = parseInt(document.getElementById("hours").value, 0);
+  const minutes = parseInt(document.getElementById("minutes").value, 0);
+
+  // console.log(days, hours, minutes);
+
+  if (days < 31 || hours < 25 || minutes < 61) {
+    start(days, hours, minutes);
+  } else {
+    document.getElementById("errorContainer").innerHTML = "Error"
+  }
+}
+
+const start = (days, hours, minutes) => {
+  let sumInSeconds = 0;
+
+  if (days) {
+    sumInSeconds = days * 86400;
+  }
+
+  if (hours) {
+    sumInSeconds = sumInSeconds + (hours * 3600);
+  }
+
+  if (minutes) {
+    sumInSeconds = sumInSeconds + (minutes * 60);
+  }
+
+  displayTime(sumInSeconds);
+}
+
+const displayTime = (seconds) => {
+
+  
+  let remainingSeconds = seconds;
+
+  const interval = setInterval(() => {
+    const days = Math.floor(remainingSeconds / (60 * 60 * 24));
+    const hours = Math.floor((remainingSeconds % (60 * 60 * 24)) / (60 * 60));
+    const minutes = Math.floor((remainingSeconds % (60 * 60)) / 60);
+
+    remainingSeconds--;
+    console.log(remainingSeconds);
+
+    document.getElementById("display").innerHTML = `${days} Дней   ${hours} Часов  ${minutes}  Минут`;
+
+    if (remainingSeconds === 0) {
+      clearInterval(interval)
+    }
+  }, 1000)
+  console.log(days, hours, minutes);
+}
+
+const setEventListeners = () => {
+  document.getElementById("start").addEventListener("click", () => {
+    handleClick();
+  })
+  
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setEventListeners();
+})
+
+
