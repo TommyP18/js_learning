a06fc81f7a6b23d2fdd0dd847ea063d1d6638ac3
Nichita <bodrugnikita18@gmail.com>
2022-08-03 18:39:15 +0300

    5,6 Tasks refatcored

--- script/ex5.js
+++ script/ex5.js
@@ -1,28 +1,28 @@
 // https://jsoneditoronline.org/#left=cloud.6dcc8b68cd7f45c1b454042d22230d91
 
-const persons = `{
-  "FIO": "Bodrug Nichita Ion",
-  "birthDate": "2000-10-18",
-  "adress": "Sevcenko 52",
-  "male": true,
-  "IDNP": 1235443534,
-  "birthPlace": "Balti, R.Moldova",
-  "contacts": {
-    "email": "bodrugnikita18@gmail.com",
-    "phoneNumber": "+37369121612",
-    "houseNumber": "+023136702"
+const persons = {
+  FIO: "Bodrug Nichita Ion",
+  birthDate: "2000-10-18",
+  adress: "Sevcenko 52",
+  male: true,
+  IDNP: 1235443534,
+  birthPlace: "Balti, R.Moldova",
+  contacts: {
+    email: "bodrugnikita18@gmail.com",
+    phoneNumber: "+37369121612",
+    houseNumber: "+023136702"
   },
-  "job": {
-    "post": "FrontEnd Developer",
-    "divisionName": "Frontend Power IT",
-    "experience": 2,
-    "sallary": 18000,
-    "hireDate": "2022-09-18",
-    "workSchedule": {
-    "workTime": "8:00 - 17:00",
-    "break": "12:00 - 13:00"
+  job: {
+    post: "FrontEnd Developer",
+    divisionName: "Frontend Power IT",
+    experience: 2,
+    sallary: 18000,
+    hireDate: "2022-09-18",
+    workSchedule: {
+    workTime: "8:00 - 17:00",
+    break: "12:00 - 13:00"
     }
   }
-}`
+}
 
-document.getElementById("jsonContainer").innerHTML = JSON.stringify(JSON.parse(persons), null, 4)
+document.getElementById("jsonContainer").innerHTML = JSON.stringify(persons, null, 4)
--- script/ex6.js
+++ script/ex6.js
@@ -1,195 +1,114 @@
-const persons = `[{
-  "FIO": "Bodrug Nichita Ion",
-  "birthDate": "2000-10-18",
-  "adress": "Sevcenko 52",
-  "male": true,
-  "IDNP": 1235443534,
-  "birthPlace": "Balti, R.Moldova",
-  "contacts": {
-    "email": "bodrugnikita18@gmail.com",
-    "phoneNumber": "+37369121612",
-    "houseNumber": "+023136702"
+const persons = [{
+  FIO: "Bodrug Nichita Ion",
+  birthDate: "2000-10-18",
+  adress: "Sevcenko 52",
+  male: true,
+  IDNP: 1235443534,
+  birthPlace: "Balti, R.Moldova",
+  contacts: {
+    email: "bodrugnikita18@gmail.com",
+    phoneNumber: "+37369121612",
+    houseNumber: "+023136702"
   },
-  "job": {
-    "post": "FrontEnd Developer",
-    "divisionName": "Frontend Power IT",
-    "experience": 2,
-    "sallary": 18000,
-    "hireDate": "2022-09-18",
-    "workSchedule": {
-    "workTime": "8:00 - 17:00",
-    "break": "12:00 - 13:00"
+  job: {
+    post: "FrontEnd Developer",
+    divisionName: "Frontend Power IT",
+    experience: 2,
+    sallary: 18000,
+    hireDate: "2022-09-18",
+    workSchedule: {
+    workTime: "8:00 - 17:00",
+    break: "12:00 - 13:00"
     }
   }
 },
 {
-  "FIO": "Bodrug Vladimir Ion",
-  "birthDate": "2006-10-18",
-  "adress": "Sevcenko 52",
-  "male": true,
-  "IDNP": 1235443534,
-  "birthPlace": "Balti, R.Moldova",
-  "contacts": {
-    "email": "bodrugnikita18@gmail.com",
-    "phoneNumber": "+37369121612",
-    "houseNumber": "+023136702"
+  FIO: "Bodrug Vladimir Ion",
+  birthDate: "2006-10-18",
+  adress: "Sevcenko 52",
+  male: true,
+  IDNP: 1235443534,
+  birthPlace: "Balti, R.Moldova",
+  contacts: {
+    email: "bodrugnikita18@gmail.com",
+    phoneNumber: "+37369121612",
+    houseNumber: "+023136702"
   },
-  "job": {
-    "post": "FrontEnd Developer",
-    "divisionName": "Frontend Power IT",
-    "experience": 2,
-    "sallary": 13000,
-    "hireDate": "2022-09-18",
-    "workSchedule": {
-    "workTime": "8:00 - 17:00",
-    "break": "12:00 - 13:00"
+  job: {
+    post: "FrontEnd Developer",
+    divisionName: "Frontend Power IT",
+    experience: 2,
+    sallary: 13000,
+    hireDate: "2022-09-18",
+    workSchedule: {
+    workTime: "8:00 - 17:00",
+    break: "12:00 - 13:00"
     }
   }
-},{
-  "FIO": "Bodrug David Ion",
-  "birthDate": "2000-10-18",
-  "adress": "Sevcenko 52",
-  "male": true,
-  "IDNP": 1235443534,
-  "birthPlace": "Balti, R.Moldova",
-  "contacts": {
-    "email": "bodrugnikita18@gmail.com",
-    "phoneNumber": "+37369121612",
-    "houseNumber": "+023136702"
+},
+{
+  FIO: "Bodrug David Ion",
+  birthDate: "2000-10-18",
+  adress: "Sevcenko 52",
+  male: true,
+  IDNP: 1235443534,
+  birthPlace: "Balti, R.Moldova",
+  contacts: {
+    email: "bodrugnikita18@gmail.com",
+    phoneNumber: "+37369121612",
+    houseNumber: "+023136702"
   },
-  "job": {
-    "post": "FrontEnd Developer",
-    "divisionName": "Frontend Power IT",
-    "experience": 2,
-    "sallary": 8000,
-    "hireDate": "2022-09-18",
-    "workSchedule": {
-    "workTime": "8:00 - 17:00",
-    "break": "12:00 - 13:00"
+  job: {
+    post: "FrontEnd Developer",
+    divisionName: "Frontend Power IT",
+    experience: 3,
+    sallary: 8000,
+    hireDate: "2022-09-18",
+    workSchedule: {
+    workTime: "8:00 - 17:00",
+    break: "12:00 - 13:00"
     }
   }
-}]`
+}]
 
-const convertedJSON = JSON.parse(persons)
+
 
 const showEmployeers = (array) => {
- const resultingArray = []
-
- for (const item of array) {
-  const {FIO, ...rest} = item
-
-  resultingArray.push(FIO)
- }
- return resultingArray
+  return array.map(({FIO}) => FIO)
 }
 
-console.log(showEmployeers(convertedJSON))
+console.log(showEmployeers(persons))
 
 const salaryAverage = (array) => {
-  const resultingArray = [];
-  
-  for (const item of array) {
-    const {
-      FIO,
-      birthdate,
-      adress,
-      male,
-      IDNP,
-      birthplace,
-      contacts,
-      job : {
-        post,
-        divisonName,
-        experience,
-        sallary
-      }
-    } = item
-    resultingArray.push(sallary)
-  }
-  return resultingArray.reduce((previous, current) => previous + current) / array.length;
+  return array.map(({job}) => job.sallary).reduce((previous, current) => previous + current) / array.length
 }
 
-console.log(salaryAverage(convertedJSON))
+console.log(salaryAverage(persons))
 
 const minSalary = (array) => {
-  const resultingArray = [];
-  for (const item of array) {
-    const {
-      FIO,
-      birthdate,
-      adress,
-      male,
-      IDNP,
-      birthplace,
-      contacts,
-      job : {
-        post,
-        divisonName,
-        experience,
-        sallary
-      }
-    } = item
-    resultingArray.push(sallary)
-  }
-  return Math.min(...resultingArray)
+  return array.map(({job}) => job.sallary).reduce((accumulator,currentValue) => {
+    return (accumulator < currentValue ? accumulator : currentValue);
+  })
 }
 
-console.log(minSalary(convertedJSON))
+console.log(minSalary(persons))
 
 const maxSalary = (array) => {
-  const resultingArray = [];
-
-  for (const item of array) {
-    const {
-      FIO,
-      birthdate,
-      adress,
-      male,
-      IDNP,
-      birthplace,
-      contacts,
-      job : {
-        post,
-        divisonName,
-        experience,
-        sallary
-      }
-    } = item
-
-    resultingArray.push(sallary)
-  }
-  return Math.max(...resultingArray)
+  return array.map(({job}) => job.sallary).reduce((accumulator,currentValue) => {
+    return (accumulator > currentValue ? accumulator : currentValue);
+  })
 }
 
-console.log(maxSalary(convertedJSON))
+console.log(maxSalary(persons))
 
 const sumSalary = (array) => {
-  const resultingArray = [];
-  for (const item of array) {
-    const {
-      FIO,
-      birthdate,
-      adress,
-      male,
-      IDNP,
-      birthplace,
-      contacts,
-      job : {
-        post,
-        divisonName,
-        experience,
-        sallary
-      }
-    } = item
-    resultingArray.push(sallary)
-  }
-  return  resultingArray.reduce((a, b) => a + b)
+  return array.map(({job}) => job.sallary).reduce((a, b) => a + b)
 }
 
-console.log(sumSalary(convertedJSON))
+console.log(sumSalary(persons))
 
-document.getElementById("employeers").innerHTML = JSON.stringify(showEmployeers(convertedJSON), null, 4)
-document.getElementById("salaryAverage").innerHTML = "Average salary is: " + salaryAverage(convertedJSON);
-document.getElementById("minSalary").innerHTML = "Minimal salary is: " + minSalary(convertedJSON);
-document.getElementById("maxSalary").innerHTML = "Maximal salary is: " + maxSalary(convertedJSON);
-document.getElementById("sumSalary").innerHTML = "Sum of the employyers salary is: " + sumSalary(convertedJSON);
+document.getElementById("employeers").innerHTML = JSON.stringify(showEmployeers(persons), null, 4)
+document.getElementById("salaryAverage").innerHTML = "Average salary is: " + salaryAverage(persons);
+document.getElementById("minSalary").innerHTML = "Minimal salary is: " + minSalary(persons);
+document.getElementById("maxSalary").innerHTML = "Maximal salary is: " + maxSalary(persons);
+document.getElementById("sumSalary").innerHTML = "Sum of the employyers salary is: " + sumSalary(persons);
