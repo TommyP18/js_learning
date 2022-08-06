f96ecf7e35889eee924d7fbfc3fe535afad0fcfa
Nichita <bodrugnikita18@gmail.com>
2022-08-07 00:55:03 +0300

    parasha

--- script/ex19.js
+++ script/ex19.js
@@ -1,42 +1,60 @@
 // REFACTOR!!!!!!!!!!
 // No function !
+// PA RA SHA
 
 const child = document.querySelector('.child');
 const parents = document.querySelectorAll('.parent');
 
-child.addEventListener('dragstart', dragStart);
-child.addEventListener('dragend', dragEnd);
+
+const dragStart = (event) => {
+  event.className += ' hold';
+
+  setTimeout(() => {
+    event.className = 'invisible'
+  }, 0);
+}
+
+const dragEnd = (event) => {
+  event.className = 'child';
+}
+
+const dragOver = (event) => {
+  event.preventDefault();
+}
+
+const dragEnter = (event) => {
+  event.preventDefault();
+  event.target.className = 'parent hovered';
+}
+
+const dragLeave = (event) => {
+  event.className = 'parent';
+}
+
+const dragDrop = (event) => {
+  event.className = 'parent';
+  event.target.append(child);
+  console.log(event.target);
+}
+
+child.addEventListener('dragstart', (e) => {
+  dragStart(e);
+});
+child.addEventListener('dragend', (e) => {
+  dragEnd(e);
+});
 
 for (const parent of parents) {
-  parent.addEventListener('dragover', dragOver);
-  parent.addEventListener('dragenter', dragEnter);
-  parent.addEventListener('dragleave', dragLeave);
-  parent.addEventListener('drop', dragDrop);
-}
-
-function dragStart() {
-  this.className += ' hold';
-  setTimeout(() => (this.className = 'invisible'), 0);
-}
-
-function dragEnd() {
-  this.className = 'child';
-}
-
-function dragOver(e) {
-  e.preventDefault();
-}
-
-function dragEnter(e) {
-  e.preventDefault();
-  this.className += ' hovered';
-}
-
-function dragLeave() {
-  this.className = 'parent';
-}
-
-function dragDrop() {
-  this.className = 'parent';
-  this.append(child);
+  parent.addEventListener('dragover', (e) => {
+    dragOver(e);
+  });
+  parent.addEventListener('dragenter', (e) => {
+    dragEnter(e);
+  });
+  parent.addEventListener('dragleave', (e) => {
+    dragLeave(e)
+  });
+  parent.addEventListener('drop', (e) => {
+    dragDrop(e);
+  });
 }
